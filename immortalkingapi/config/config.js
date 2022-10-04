19b74f11fc715377c5fb04b7e56cc7ac8669f396
sauer93n <sauer93n@gmail.com>
2022-10-04 12:57:18 +0300

    jwt secrets added

--- immortalkingapi/config/config.js
+++ immortalkingapi/config/config.js
@@ -2,4 +2,6 @@ require('dotenv').config({path: __dirname + '/.env'});
 
 module.exports = {
     DB_HOST: process.env.DB_HOST,
+    JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
+    JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
 }
